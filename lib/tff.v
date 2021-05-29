d0662b815acaf7bd60669542bef951d4dac92165
Matthew Dupree <mattjdupree@gmail.com>
2021-05-29 09:37:10 -0700

    Add carry to TFF -- not happy with it, but it works'.

--- lib/tff.v
+++ lib/tff.v
@@ -1,29 +1,35 @@
 `timescale 1ns/100ps
-//https://arch.library.northwestern.edu/downloads/2514nk901?locale=en
-module tff(WE,RE,rstb,out);
+// https://arch.library.northwestern.edu/concern/generic_works/8k71nh39w?locale=en
+module tff(WE,RE,rstb,out,carry);
     parameter BITS=2;
     parameter RING_SEGS=2**BITS;
 
     input WE,RE,rstb;
     output out;
+    output reg carry;
 
     wire EN = WE | RE;
     wire ring_ctrl = rstb & ~RE;
     wire ring_loopback = (ring_ctrl ~& ring[0]);
 
-    reg [RING_SEGS:0] ring;
+    reg [RING_SEGS-1:0] ring;
     
     always @(posedge EN) begin
         #0.1
         while(EN & rstb) begin
-            ring <= {ring_loopback,ring[RING_SEGS:1]};
+            ring <= {ring_loopback,ring[RING_SEGS-1:1]};
             #1;
         end
     end
 
     assign out=~ring[0];
 
+    always @(posedge out) begin
+        if(rstb)
+            carry<=1'b1;
+    end
+
     always @(negedge rstb) begin
-        ring <=~0;
+        carry<=1'b0;
     end
 endmodule
--- tests/tff_tb.v
+++ tests/tff_tb.v
@@ -2,33 +2,44 @@
 module tff_tb();
     reg rstb=1, WE=0, RE=0;
     wire out;
+    wire carry;
 
-    tff DUT(WE,RE,rstb,out);
+    tff DUT(WE,RE,rstb,out,carry);
+    defparam DUT.RING_SEGS=59;
+
+    task write(input integer n); begin
+        WE<=1;
+        #(n);
+        WE<=0;
+        #1;
+    end endtask
+
+    task read(input integer n); begin
+        RE<=1;
+        #(n);
+        RE<=0;
+        rstb<=0;
+        #1;
+        rstb<=1;
+    end endtask
 
     initial begin
         $dumpfile(`OUTFILE);
         $dumpvars(0);
-        // Reset phase
-        rstb<=1;
+        // Read phase
+        read(60);
         #1;
-        rstb<=0;
-        #1;
-        rstb<=1;
 
         // Write phase
-        #1;
-        WE<=1;
-        #2;
-        WE<=0;
-        #2;
-        WE<=1;
-        #1;
-        WE<=0;
-        #2;
+        write(5);
+        write(7);
+        write(3);
+        write(0);
+        write(44);
 
         // Read phase
-        RE<=1;
-        #5;
+        read(60);
+        #0;
         rstb<=0;
         #1;
         rstb<=1;
